6d6081ff543acb19315ccaca541abad710cf31ab
anttonorpana <antton.orpana@gmail.com>
2026-02-24 15:17:27 +0200

    docs: comment for print

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ public class helloworld {
     // main class
     public static void main(String[] args) {
         // main method
-        System.out.println("Hello world");
+        System.out.println("Hello world"); // what is printed
     }
 }
