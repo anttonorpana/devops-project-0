67caaa8e4335898adeb3f078fadf71e4d79c334f
anttonorpana <antton.orpana@gmail.com>
2026-02-24 15:21:45 +0200

    feat: added name string (Pena)

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,8 @@ public class helloworld {
     // main class
     public static void main(String[] args) {
         // main method
+
+        String name = "Pena";
         System.out.println("Hello world"); // what is printed
-    }
-}
+
+    
