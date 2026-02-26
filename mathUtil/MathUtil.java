f72dbe57c6561928ce25197f6896bb8f0bfc1736
Tuke13 <tuukka.santanen@gmail.com>
2026-02-26 14:10:16 +0200

    style(MathUtil.java): change method order for better readability

--- mathUtil/MathUtil.java
+++ mathUtil/MathUtil.java
@@ -3,20 +3,20 @@ public class MathUtil {
         return a >= b ? a : b;
     }
 
-    public int smaller(int a, int b) {
-        return a <= b ? a : b;
-    }
-
     public float bigger(float a, float b) {
         return a >= b ? a : b;
     }
 
-    public float smaller(float a, float b) {
+    public double bigger(double a, double b) {
+        return a >= b ? a : b;
+    }
+
+    public int smaller(int a, int b) {
         return a <= b ? a : b;
     }
 
-    public double bigger(double a, double b) {
-        return a >= b ? a : b;
+    public float smaller(float a, float b) {
+        return a <= b ? a : b;
     }
 
     public double smaller(double a, double b) {
