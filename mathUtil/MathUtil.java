632b2679ea987199b4f81ceaec6d1d604f8a0ff4
Tuke13 <tuukka.santanen@gmail.com>
2026-02-26 14:04:14 +0200

    feat(MathUtil.java): add smaller(int a, int b) method

--- mathUtil/MathUtil.java
+++ mathUtil/MathUtil.java
@@ -2,4 +2,8 @@ public class MathUtil {
     public int bigger(int a, int b) {
         return a > b ? a : b;
     }
+
+    public int smaller(int a, int b) {
+        return a > b ? a : b;
+    }
 }
