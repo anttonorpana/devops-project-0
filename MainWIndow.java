489a54abbe43ecf3ab6004d73f0eb01be6eb67da
Ilmari Takkunen <takkunen.ilmari@gmail.com>
2026-02-27 17:57:42 +0200

    feat: add button to window

--- MainWIndow.java
+++ MainWIndow.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
+    private JButton button;
+
     MainWindow() {
         setSize(300, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        button = new JButton("click me");
+
     }
 }
