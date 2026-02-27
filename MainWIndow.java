e5e943aa8c56b0c00aeb1033778985724a1a8e17
Ilmari Takkunen <takkunen.ilmari@gmail.com>
2026-02-27 18:17:10 +0200

    docs: add documentation for MainWindow class

--- MainWIndow.java
+++ MainWIndow.java
@@ -1,18 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Window for our app
+ */
 public class MainWindow extends JFrame {
     private JButton button;
 
     MainWindow() {
         setSize(300, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
         setLayout(new FlowLayout());
-
         button = new JButton("click me");
-
         add(button);
-
     }
 }
