bdb979cce9b2c52599ec12cb51167ccca03392aa
Kimmo <niko.karhu02@gmail.com>
2026-02-27 18:17:44 +0200

    helloworld.java Printtaaja method

--- helloworld.java
+++ helloworld.java
@@ -2,9 +2,11 @@ public class helloworld {
     // main class
     public static void main(String[] args) {
         // main method
+        Printtaaja();
+    }
 
+    public static void Printtaaja() {
         String name = "Esko";
         System.out.println("Hello, " + name); // what is printed
-
     }
 }
