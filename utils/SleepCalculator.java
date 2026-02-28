f6c225355fc150973f5277eba5316691c3e16cf0
Ilmari Takkunen <takkunen.ilmari@gmail.com>
2026-02-28 18:44:29 +0200

    feat: add Printing to calculateSleepTime method

--- utils/SleepCalculator.java
+++ utils/SleepCalculator.java
@@ -15,5 +15,10 @@ public class SleepCalculator {
         Double sleptTimeYears = 0.3333 * age;
         Double sleptTimeDays = 0.3333 * ageInDays;
         Double sleptTimeHours = 0.333 * ageInHours;
+
+        System.out.println("You have slept:");
+        System.out.println(sleptTimeYears + " years");
+        System.out.println(sleptTimeDays + " days");
+        System.out.println(sleptTimeHours + " hours");
     }
 }
