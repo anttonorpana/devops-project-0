c6fb3dc2212e4c5bda7bd6256300ce58cfc4cae7
Ilmari Takkunen <takkunen.ilmari@gmail.com>
2026-02-28 18:31:27 +0200

    feat: add age attribute to calculateSleepTime method

--- utils/SleepCalculator.java
+++ utils/SleepCalculator.java
@@ -1,7 +1,12 @@
 package utils;
 
+import java.io.Console;
+
 public class SleepCalculator {
     public static void CalculateSleepTime() {
+        Console c = System.console();
 
+        System.out.println("How old are you? Give anwer in whole years: ");
+        int age = Integer.parseInt(c.readLine());
     }
 }
