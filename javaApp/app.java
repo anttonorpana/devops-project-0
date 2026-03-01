ec8c67796ae1e76d5469acc4cbcc74b2a9f4f442
Kimmo <niko.karhu02@gmail.com>
2026-03-01 10:31:54 +0200

    fix(app): handle null name in greeting

--- javaApp/app.java
+++ javaApp/app.java
@@ -6,6 +6,9 @@ public class app {
     }
 
     public static String getGreetingMessage(String name) {
+        if (name == null || name.isBlank()) {
+            name = "Developer";
+        }
         return "Hello, " + name + "!";
     }
 }
