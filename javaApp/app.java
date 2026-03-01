bf3e6f436b529ffd0e0dd94a670b7ba6ffd7e79b
Kimmo <niko.karhu02@gmail.com>
2026-03-01 10:28:21 +0200

    feat(app): add greeting method

--- javaApp/app.java
+++ javaApp/app.java
@@ -1,7 +1,11 @@
-package com.example;
+package javaApp;
 
-public class App {
+public class app {
     public static void main(String[] args) {
-        System.out.println("App started");
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello, DevOps!";
     }
 }
