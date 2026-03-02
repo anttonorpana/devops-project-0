42cf9ddc03c83cc17337ef52e9e35e11fe3b3bf0
ilmaritak <takkunen.ilmari@gmail.com>
2026-03-02 19:21:59 +0200

    feat: add calculateWinner method

--- utils/RockPaperScissors.java
+++ utils/RockPaperScissors.java
@@ -4,10 +4,10 @@ import java.io.Console;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RockPaperScissors {
-    final int rockIndex = 0;
-    final int paperIndex = 1;
-    final int scissorIndex = 2;
-    final Console c = System.console();
+    private final int rockIndex = 0;
+    private final int paperIndex = 1;
+    private final int scissorIndex = 2;
+    private final Console c = System.console();
 
     public void playRockPaperScissors() {
         startGame();
@@ -25,6 +25,10 @@ public class RockPaperScissors {
                 yourIndex = scissorIndex;
                 break;
         }
+
+        int pcIndex = getRandomIndex(0, 2);
+
+        calculateWinner(yourIndex, pcIndex);
     }
 
     public int getRandomIndex(int min, int max) {
@@ -34,5 +38,9 @@ public class RockPaperScissors {
     public void startGame() {
         System.out.println("Welcome to the game of rock paper sciccors!");
         System.out.println("Type eiher <rock>, <paper> or <scissors>");
+    }
+
+    public void calculateWinner(int index1, int index2) {
+
     }
 }
