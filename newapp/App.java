7e1a3f096831b7c2aa74d5d356925f316be97087
Kimmo <niko.karhu02@gmail.com>
2026-03-02 13:50:02 +0200

    feat(app): add greeting method

--- newapp/App.java
+++ newapp/App.java
@@ -5,6 +5,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("App started");
+        System.out.println(greet());
+    }
+
+    public static String greet() {
+        return "Hello!";
     }
 }
