9afbbb50fa180f0c7c9cc0e9d60637a06be03dea
Tuke13 <tuukka.santanen@gmail.com>
2026-03-02 20:40:44 +0200

    feat(ExamplePrints.java): add examples how to use the util/prints methods

--- ExamplePrints.java
+++ ExamplePrints.java
@@ -1,9 +1,11 @@
 import static utils.Prints.print;
 import static utils.Prints.println;
-import static utils.Prints.println;
 
 public class ExamplePrints {
     public static void main(String[] args) {
-        
+        print("I want to ");
+        println("eat ice cream");
+        println();
+        println("What do you want to eat?");
     }
 }
