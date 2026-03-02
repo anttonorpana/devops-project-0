9a9e3ff22de4cd0e08617c9f06f087b3977c5c49
Tuke13 <tuukka.santanen@gmail.com>
2026-03-02 20:43:21 +0200

    docs(ExamplePrints.java): add comments how the util/prints methods work

--- ExamplePrints.java
+++ ExamplePrints.java
@@ -3,9 +3,9 @@ import static utils.Prints.println;
 
 public class ExamplePrints {
     public static void main(String[] args) {
-        print("I want to ");
-        println("eat ice cream");
-        println();
-        println("What do you want to eat?");
+        print("I want to "); // This print doesn't make newline at the end
+        println("eat ice cream"); // So this continues in the same line
+        println(); // This prints nothing but makes a newline at the end
+        println("What do you want to eat?"); // So this will be seperated from the upper ones
     }
 }
