2b9a3499ff7d84be7780e1ce5e2edc1edc307372
Tuke13 <tuukka.santanen@gmail.com>
2026-03-02 20:37:56 +0200

    feat(ExamplePrints.java): add ExamplePrints.java with class and main method

---
+++ ExamplePrints.java
@@ -0,0 +1,9 @@
+import static utils.Prints.print;
+import static utils.Prints.println;
+import static utils.Prints.println;
+
+public class ExamplePrints {
+    public static void main(String[] args) {
+        
+    }
+}
